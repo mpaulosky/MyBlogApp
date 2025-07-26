1f8bc3b14d929139cc452bdfef9d2b029d1dadb3
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-07-26 12:10:32 -0700

    Add `.editorconfig` for consistent code styling and include `.npmignore` alongside `package-lock.json` for dependency management.

---
+++ Shared/Services.cs
@@ -0,0 +1,14 @@
+﻿namespace Shared;
+
+public static class Services
+{
+
+	public const string SERVER = "Publication-Server";
+
+	public const string DATABASE = "PublicationDb";
+
+	public const string WEBSITE = "Web";
+
+	public const string CACHE = "RedisCache";
+
+}
