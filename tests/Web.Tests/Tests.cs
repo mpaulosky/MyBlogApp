c3b4f02d6f82813d30a2f5b73745a044969d1b9d
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-07-27 08:17:03 -0700

    Add placeholder `Tests` class to `Web.Tests` for future implementation

---
+++ tests/Web.Tests/Tests.cs
@@ -0,0 +1,17 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     Tests.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : MyBlogApp
+// Project Name :  Web.Tests
+// =======================================================
+
+namespace Web;
+
+public class Tests
+{
+
+	// ToDo: Implement tests for the Result class.
+
+}
