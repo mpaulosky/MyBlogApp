c7414b569441a5fc9fa07eddc5a2efc798a04c92
mpaulosky <60372079+mpaulosky@users.noreply.github.com>
2025-07-29 21:50:01 -0700

    Add `GlobalUsings.cs` to centralize common using directives in the `Web` project

---
+++ Web/GlobalUsings.cs
@@ -0,0 +1,23 @@
+// =======================================================
+// Copyright (c) 2025. All rights reserved.
+// File Name :     GlobalUsings.cs
+// Company :       mpaulosky
+// Author :        Matthew
+// Solution Name : MyBlogApp
+// Project Name :  Web
+// =======================================================
+
+global using Auth0.AspNetCore.Authentication;
+
+global using FluentValidation;
+
+global using Microsoft.AspNetCore.Authentication;
+global using Microsoft.AspNetCore.Authentication.Cookies;
+
+global using ServiceDefaults;
+
+global using Web.Components;
+global using Web.Data;
+global using Web.Data.Entities;
+
+global using static Shared.Services;
